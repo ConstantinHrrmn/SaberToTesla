7834adb30f9d653d83cfb1b602f9b8d2e26ec702
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2022-05-11 14:55:29 +0200

    Création projet test pour la recherche et le telechargement de fichiers Bsaber

---
+++ Code/SaberSearchAndDownload/SaberSearchAndDownload/ApiManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Dynamic;
+using Newtonsoft.Json.Converters;
+
+namespace SaberSearchAndDownload
+{
+    public class ApiManager
+    {
+        private const string BASE_URL = "https://api.beatsaver.com/";
+        private const string MAPS_URL = BASE_URL + "maps/";
+        
+        public static List<Map> GetLatestMaps()
+        {
+            string url = MAPS_URL + "latest";
+            WebClient client = new WebClient();
+            var json = client.DownloadString(url);
+            dynamic maps = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine(maps.docs[0].id);
+
+            return null;
+        }
+
+    }
+}
---
+++ Code/SaberSearchAndDownload/SaberSearchAndDownload/Map.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaberSearchAndDownload
+{
+    public class Map
+    {
+        private string id;
+        private string name;
+        private string description;
+        private string imagePath;
+        private string songPath;
+        private string downloadPath;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public string ImagePath { get => imagePath; set => imagePath = value; }
+        public string SongPath { get => songPath; set => songPath = value; }
+        public string DownloadPath { get => downloadPath; set => downloadPath = value; }
+
+        public Map(string id, string name, string description, string imagePath, string songPath, string downloadPath)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.imagePath = imagePath;
+            this.songPath = songPath;
+            this.downloadPath = downloadPath;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
